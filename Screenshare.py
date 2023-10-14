2b7c04aeac09133b2fa8c1d644007c9a8d700dea
Tudor Voie <tudor_voie@yahoo.com>
2023-10-14 13:22:56 +0300

    Add files via upload

---
+++ Screenshare.py
@@ -0,0 +1,12 @@
+import pyautogui
+from time import sleep
+pyautogui.click(507,1055)
+#sleep(5)
+pyautogui.click(171,964)
+#sleep(5)
+pyautogui.click(853,419)
+#sleep(5)
+sleep(1)
+pyautogui.click(796,535)
+sleep(2)
+pyautogui.click(1150,800)
---
+++ screensharestop.py
@@ -0,0 +1,5 @@
+import pyautogui
+from time import sleep
+pyautogui.click(507,1055)
+sleep(2)
+pyautogui.click(1819,239)
